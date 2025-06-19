0a43b36032c03dcd0141564048c640b84867e482
alvaromsan <alvaro.marinsan@gmail.com>
2025-06-19 13:54:50 +0200

    Added READ, UPDATE, DELETE Logistic center endpoints. Added Center Status & Capicity validity checks

--- src/main/java/com/hackathon/inditex/Controllers/CenterManagementController.java
+++ src/main/java/com/hackathon/inditex/Controllers/CenterManagementController.java
@@ -1,14 +1,14 @@
 package com.hackathon.inditex.Controllers;
 
 import com.hackathon.inditex.DTO.CenterRequest;
+import com.hackathon.inditex.Entities.Center;
 import com.hackathon.inditex.Services.CenterManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/centers")
@@ -17,10 +17,27 @@ public class CenterManagementController {
     @Autowired
     private CenterManagementService centerManagementService;
 
-
     @PostMapping
     public ResponseEntity<?> createNewCenter(@RequestBody CenterRequest centerRequest) {
         String responseMessage = centerManagementService.createNewCenter(centerRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseMessage);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> readAllCenters() {
+        List<Center> centerList = centerManagementService.readAllCenters();
+        return ResponseEntity.ok(centerList);
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<?> updateCenter(@PathVariable Long id, @RequestBody CenterRequest centerRequest) {
+        String responseMessage = centerManagementService.updateCenter(id, centerRequest);
+        return ResponseEntity.ok(responseMessage);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteCenter(@PathVariable Long id) {
+        String responseMessage = centerManagementService.deleteCenter(id);
+        return ResponseEntity.ok(responseMessage);
     }
 }
--- src/main/java/com/hackathon/inditex/DTO/CenterRequest.java
+++ src/main/java/com/hackathon/inditex/DTO/CenterRequest.java
@@ -3,14 +3,12 @@ package com.hackathon.inditex.DTO;
 import com.hackathon.inditex.Entities.Coordinates;
 import lombok.Data;
 
-import java.util.List;
-
 @Data // generates getters, setters, toString, equals, hashCode, etc.
 public class CenterRequest {
     private String name;
     private String capacity; // Accepts strings like "B", "MS", "BMS"
     private String status;   // Accepts "AVAILABLE" or "ASSIGNED"
-    private int maxCapacity;
-    private int currentLoad;
+    private Integer maxCapacity;
+    private Integer currentLoad;
     private Coordinates coordinates;
 }
