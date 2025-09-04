478478c242f35472bef39377d8f56daf2bd618d5
alvaromsan <alvaro.marinsan@gmail.com>
2025-09-04 11:18:37 +0200

    CenterManagementController - added comments

--- src/main/java/com/hackathon/inditex/Controllers/CenterManagementController.java
+++ src/main/java/com/hackathon/inditex/Controllers/CenterManagementController.java
@@ -18,7 +18,7 @@ import java.util.List;
 @RequestMapping("/api/centers")
 public class CenterManagementController {
 
-    // Autowiring the centerManagementService bean from the ApplicationContext
+    // Autowiring the CenterManagementService bean from the ApplicationContext
     @Autowired
     private CenterManagementService centerManagementService;
 
