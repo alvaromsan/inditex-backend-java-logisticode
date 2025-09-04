9d4fa89315b258631a3e048aad268c7a9e2c32fc
alvaromsan <alvaro.marinsan@gmail.com>
2025-09-04 10:53:08 +0200

    HealthCheckController commented

--- src/main/java/com/hackathon/inditex/Controllers/HealthCheckController.java
+++ src/main/java/com/hackathon/inditex/Controllers/HealthCheckController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * REST Controller for checking if the container is working properly.
- * Define a health-check endpoint.
+ * Defines a health-check endpoint.
  */
 @RestController
 @RequestMapping("/health")
