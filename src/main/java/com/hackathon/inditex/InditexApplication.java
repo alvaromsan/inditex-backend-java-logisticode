110a491db73aba76a900213cf70247cdea2303cd
alvaromsan <alvaro.marinsan@gmail.com>
2025-09-04 14:25:34 +0200

    InditexApp - added Javadoc comments

--- src/main/java/com/hackathon/inditex/InditexApplication.java
+++ src/main/java/com/hackathon/inditex/InditexApplication.java
@@ -3,6 +3,11 @@ package com.hackathon.inditex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point for the Inditex Spring Boot application.
+ *
+ * This class bootstraps the Spring context and starts the application.
+ */
 @SpringBootApplication
 public class InditexApplication {
 
